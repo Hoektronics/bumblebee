36e9bf06e140a5ced4d09a67165a5aea959cc4a8
Justin Nesselrotte <jnesselr@harding.edu>
2014-09-08 02:15:33 -0500

    Updating minor install stuff

--- raspi/install.sh
+++ raspi/install.sh
@@ -8,18 +8,15 @@ sudo apt-get install fswebcam uvcdynctrl v4l-utils python-picamera
 
 #get Botqueue linked up and working on boot.
 sudo apt-get install -qy git-core vim screen python-pip
-git clone https://github.com/Hoektronics/bumblebee.git
 sudo usermod -a -G dialout pi
 sudo pip install pyserial Pygments requests requests-oauth
 
 #make botqueue start on boot 
-sudo /bin/sh -c 'cat /home/pi/BotQueue/bumblebee/raspi/inittab >> /etc/inittab'
-chmod a+x $HOME/BotQueue/bumblebee/raspi/bin/bumblebee
-cat $HOME/BotQueue/bumblebee/raspi/profile >> $HOME/.profile
-source $HOME/.profile
+sudo /bin/sh -c 'cat /home/pi/bumblebee/raspi/inittab >> /etc/inittab'
+cat $HOME/bumblebee/raspi/profile >> $HOME/.profile
 
 cd ..
-python setup.py install
+git submodule update --init
 
 #authorize our app now.
-screen -dR botqueue bumblebee
+screen -dR botqueue python -m bumblebee
