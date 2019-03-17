43a2b1133e6fc25ceef2ad71d64fca5916ac346f
Justin Nesselrotte <admin@jnesselr.org>
2019-03-17 01:59:49 -0600

    Refactored FinishJob command to use BotQueueApi
    Signed-off-by: Justin Nesselrotte <admin@jnesselr.org>

--- bumblebee/host/api/commands/finish_job.py
+++ bumblebee/host/api/commands/finish_job.py
@@ -1,35 +1,23 @@
-from bumblebee.host.api.rest import RestApi
+from bumblebee.host.api.botqueue_api import BotQueueApi
 from bumblebee.host.events import JobEvents
 from bumblebee.host.types import Job
 
 
 class FinishJob(object):
     def __init__(self,
-                 api: RestApi):
+                 api: BotQueueApi):
         self.api = api
 
     def __call__(self, job_id):
-        url = f"/host/jobs/{job_id}"
-
-        token_api = self.api.with_token()
-        response = token_api.put(url, {
-            "status": "quality_check"
+        response = self.api.command("FinishJob", {
+            "data": job_id
         })
-
-        if not response.ok:
-            return
-
-        job_response = token_api.get(url)
-
-        if not job_response.ok:
-            return
-
-        json = job_response.json()
+        
         job = Job(
-            id=json["data"]["id"],
-            name=json["data"]["name"],
-            status=json["data"]["status"],
-            file_url=json["data"]["url"]
+            id=response["id"],
+            name=response["name"],
+            status=response["status"],
+            file_url=response["url"]
         )
 
         JobEvents.JobFinished(job).fire()
--- tests/host/api/commands/test_finish_job.py
+++ tests/host/api/commands/test_finish_job.py
@@ -1,9 +1,7 @@
-from unittest.mock import MagicMock, PropertyMock
+from unittest.mock import Mock
 
-from requests import Response
-
+from bumblebee.host.api.botqueue_api import BotQueueApi
 from bumblebee.host.api.commands.finish_job import FinishJob
-from bumblebee.host.api.rest import RestApi
 from bumblebee.host.events import JobEvents
 
 
@@ -11,42 +9,22 @@ class TestFinishJob(object):
     def test_finishing_job(self, resolver, fakes_events):
         fakes_events.fake(JobEvents.JobFinished)
 
-        job_start_response = MagicMock(Response)
-        job_start_ok_mock = PropertyMock(return_value=True)
-        type(job_start_response).ok = job_start_ok_mock
-        job_start_response.json.return_value = {}
-
-        job_show_response = MagicMock(Response)
-        job_show_ok_mock = PropertyMock(return_value=True)
-        type(job_show_response).ok = job_show_ok_mock
-        job_show_response.json.return_value = {
-            "data": {
+        api = Mock(BotQueueApi)
+        api.command.return_value = {
                 "id": 1,
                 "name": "My Job",
                 "status": "in_progress",
                 "url": "file_url"
             }
-        }
-
-        api = MagicMock(RestApi)
-        api.with_token.return_value = api
-        api.put.return_value = job_start_response
-        api.get.return_value = job_show_response
         resolver.instance(api)
 
         start_job = resolver(FinishJob)
 
         start_job(1)
 
-        api.put.assert_called_with("/host/jobs/1", {
-            "status": "quality_check"
+        api.command.assert_called_once_with("FinishJob", {
+            "data": 1
         })
-        api.get.assert_called_with("/host/jobs/1")
-
-        api.with_token.assert_called()
-        job_start_ok_mock.assert_called()
-        job_show_ok_mock.assert_called()
-        job_show_response.json.assert_called()
 
         assert fakes_events.fired(JobEvents.JobFinished).once()
 
